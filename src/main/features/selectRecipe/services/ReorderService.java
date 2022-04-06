8a4d2c465ca483a0996c9888e75f6572b2298e92
Andrew Tran <Andrew@synercon.co>
2022-04-06 18:00:29 +1000

    add code to sort recipes

---
+++ src/main/features/selectRecipe/services/ReorderService.java
@@ -0,0 +1,48 @@
+package features.selectRecipe.services;
+
+import models.fridge.Fridge;
+import models.fridge.Item;
+import models.recipeList.Recipe;
+import models.recipeList.Recipes;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ReorderService {
+    public Recipes reorderRecipeList(ArrayList<Recipe> recipeList, Fridge fridge) {
+        Fridge sortedFridge = reorderItemsInFridge(fridge);
+        ArrayList<Recipe> sortedRecipeList = new ArrayList<>();
+        for(Item item : sortedFridge.getItems()){
+            int index = sortedFridge.getItems().indexOf(item);
+            Recipe matchingRecipe = findRecipe(recipeList,index,sortedFridge.getItems());
+            if (matchingRecipe != null){
+                sortedRecipeList.add(matchingRecipe);
+            }
+
+        }
+        Recipes result = new Recipes();
+        result.setRecipes(sortedRecipeList);
+        return result;
+    }
+
+    private Recipe findRecipe(ArrayList<Recipe> recipeList, int index, ArrayList<Item> items) {
+        if (index > items.size() - 1){
+            return null;
+        }
+        List<Recipe> filteredRecipeList = recipeList.stream().filter(recipe -> !recipe.getChosen() && recipe.getIngredients().stream().anyMatch(ingredient -> ingredient.getItem() == items.get(index).getItem())).collect(Collectors.toList());
+        if (filteredRecipeList.size() == 1){
+            filteredRecipeList.get(0).setChosen(true);
+            return filteredRecipeList.get(0);
+        }
+        return findRecipe(recipeList,index + 1, items);
+    }
+
+    public Fridge reorderItemsInFridge(Fridge fridge){
+        Fridge fridgeWithSortedItems = new Fridge();
+        ArrayList<Item> sortedItems = new ArrayList<>(fridge.getItems().stream().sorted(Comparator.comparing(Item::getUseBy)).collect(Collectors.toList()));
+        fridgeWithSortedItems.setItems(sortedItems);
+        return fridgeWithSortedItems;
+    }
+}
--- src/main/features/selectRecipe/tests/SelectRecipeServiceTest.java
+++ src/main/features/selectRecipe/tests/SelectRecipeServiceTest.java
@@ -1,19 +1,14 @@
 import features.selectRecipe.services.SelectRecipeService;
-import models.IngredientUnit;
 import models.OperationResultMessage;
 import models.OperationResultStatus;
 import models.fridge.Fridge;
-import models.fridge.Item;
-import models.recipeList.Ingredient;
 import models.recipeList.Recipe;
 import models.recipeList.Recipes;
-import org.jetbrains.annotations.Contract;
-import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import testData.selectRecipesTestData;
 
-import java.time.LocalDate;
 import java.util.ArrayList;
 
 @SpringBootTest(classes = SelectRecipeServiceTest.class)
@@ -21,8 +16,8 @@ public class SelectRecipeServiceTest {
     @Test
     public void canFindRecipe() {
         SelectRecipeService service = new SelectRecipeService();
-        Fridge fridge = getFridgeData();
-        Recipes recipeList = getRecipeList();
+        Fridge fridge = selectRecipesTestData.getFridgeData();
+        Recipes recipeList = selectRecipesTestData.getRecipeList();
         OperationResultMessage<ArrayList<Recipe>> selectedRecipeResult = service.findRecipe(fridge, recipeList);
         Assertions.assertEquals(OperationResultStatus.SUCCESS, selectedRecipeResult.getStatus());
         Assertions.assertEquals(1, selectedRecipeResult.getResult().size());
@@ -32,8 +27,8 @@ public class SelectRecipeServiceTest {
     @Test
     public void canFindRecipes() {
         SelectRecipeService service = new SelectRecipeService();
-        Fridge fridge = getFridgeData2();
-        Recipes recipeList = getRecipeList2();
+        Fridge fridge = selectRecipesTestData.getFridgeData2();
+        Recipes recipeList = selectRecipesTestData.getRecipeList2();
         OperationResultMessage<ArrayList<Recipe>> selectedRecipeResult = service.findRecipe(fridge, recipeList);
         Assertions.assertEquals(OperationResultStatus.SUCCESS, selectedRecipeResult.getStatus());
         Assertions.assertEquals(2, selectedRecipeResult.getResult().size());
@@ -42,157 +37,12 @@ public class SelectRecipeServiceTest {
     @Test
     public void cannotFindRecipe() {
         SelectRecipeService service = new SelectRecipeService();
-        Fridge fridge = getFridgeData3();
-        Recipes recipeList = getRecipeList2();
+        Fridge fridge = selectRecipesTestData.getFridgeData3();
+        Recipes recipeList = selectRecipesTestData.getRecipeList2();
         OperationResultMessage<ArrayList<Recipe>> selectedRecipeResult = service.findRecipe(fridge, recipeList);
         Assertions.assertEquals(OperationResultStatus.EMPTY, selectedRecipeResult.getStatus());
         Assertions.assertEquals(null, selectedRecipeResult.getResult());
     }
 
-
-    private Recipes getRecipeList() {
-        ArrayList<Recipe> recipes = new ArrayList<>();
-        recipes.add(getGrilledCheeseOnToast());
-        recipes.add(getSaladSandwich());
-        Recipes result = new Recipes();
-        result.setRecipes(recipes);
-        return result;
-    }
-
-    private Recipes getRecipeList2() {
-        ArrayList<Recipe> recipes = new ArrayList<>();
-
-        recipes.add(getHamAndCheeseToastie());
-        recipes.add(getChickenSalad());
-        recipes.add(getScrambledEgg());
-
-        Recipes result = new Recipes();
-        result.setRecipes(recipes);
-        return result;
-    }
-
-    private Recipe getHamAndCheeseToastie() {
-        return new Recipe("hamandcheesetoastie", getHamAndCheeseToastieIngredients());
-    }
-
-    private Recipe getChickenSalad() {
-        return new Recipe("chickensalad", getChickenSaladIngredients());
-    }
-
-
-    private @NotNull Recipe getSaladSandwich() {
-        return new Recipe("saladsandwich", getSaladSandwichIngredients());
-    }
-
-    private Recipe getGrilledCheeseOnToast() {
-        return new Recipe("grilledcheeseontoast", getGrilledCheeseOnToastIngredients());
-    }
-
-    private Recipe getScrambledEgg() {
-        return new Recipe("scrambledegg", getScrambledEggIngredients());
-    }
-
-    private ArrayList<Ingredient> getScrambledEggIngredients() {
-        Ingredient cheese = new Ingredient("cheese", 2, IngredientUnit.SLICES);
-        Ingredient eggs = new Ingredient("egg", 4, IngredientUnit.OF);
-        ArrayList<Ingredient> result = new ArrayList<>();
-        result.add(cheese);
-        result.add(eggs);
-        return result;
-    }
-
-    private ArrayList<Ingredient> getChickenSaladIngredients() {
-        Ingredient mixedSalad = new Ingredient("mixed salad", 200, IngredientUnit.GRAMS);
-        Ingredient chicken = new Ingredient("chicken", 300, IngredientUnit.GRAMS);
-        ArrayList<Ingredient> result = new ArrayList<>();
-        result.add(mixedSalad);
-        result.add(chicken);
-        return result;
-    }
-
-    private ArrayList<Ingredient> getHamAndCheeseToastieIngredients() {
-        Ingredient bread = new Ingredient("bread", 2, IngredientUnit.SLICES);
-        Ingredient ham = new Ingredient("ham", 2, IngredientUnit.SLICES);
-        Ingredient cheese = new Ingredient("cheese", 2, IngredientUnit.SLICES);
-        ArrayList<Ingredient> result = new ArrayList<>();
-        result.add(bread);
-        result.add(ham);
-        result.add(cheese);
-        return result;
-    }
-
-    private ArrayList<Ingredient> getSaladSandwichIngredients() {
-        Ingredient bread = new Ingredient("bread", 2, IngredientUnit.SLICES);
-        Ingredient mixedSalad = new Ingredient("mixed salad", 200, IngredientUnit.GRAMS);
-        ArrayList<Ingredient> result = new ArrayList<>();
-        result.add(bread);
-        result.add(mixedSalad);
-        return result;
-    }
-
-
-    private ArrayList<Ingredient> getGrilledCheeseOnToastIngredients() {
-        Ingredient bread = new Ingredient("bread", 2, IngredientUnit.SLICES);
-        Ingredient cheese = new Ingredient("cheese", 2, IngredientUnit.SLICES);
-        ArrayList<Ingredient> result = new ArrayList<>();
-        result.add(bread);
-        result.add(cheese);
-        return result;
-    }
-
-    private Fridge getFridgeData() {
-        Fridge result = new Fridge();
-        result.setItems(GetItemList());
-        return result;
-    }
-
-    private Fridge getFridgeData2() {
-        Fridge result = new Fridge();
-        result.setItems(GetItemList2());
-        return result;
-    }
-
-    private Fridge getFridgeData3() {
-        Fridge result = new Fridge();
-        result.setItems(new ArrayList<>());
-        return result;
-    }
-
-    private ArrayList<Item> GetItemList2() {
-        ArrayList<Item> result = new ArrayList<>();
-        Item bread = new Item("bread", 10, IngredientUnit.SLICES, LocalDate.now().plusDays(3));
-        Item cheese = new Item("cheese", 10, IngredientUnit.SLICES, LocalDate.now().plusDays(5));
-        Item butter = new Item("butter", 250, IngredientUnit.GRAMS, LocalDate.now().plusMonths(3));
-        Item peanutButter = new Item("peanut butter", 250, IngredientUnit.GRAMS, LocalDate.now().plusYears(2));
-        Item mixedSalad = new Item("mixed salad", 500, IngredientUnit.GRAMS, LocalDate.now().plusDays(2));
-        Item ham = new Item("ham", 2, IngredientUnit.SLICES, LocalDate.now().plusDays(5));
-        Item eggs = new Item("egg", 5, IngredientUnit.OF, LocalDate.now().plusDays(7));
-        Item chicken = new Item("chicken", 300, IngredientUnit.GRAMS, LocalDate.now().minusDays(2));
-
-        result.add(bread);
-        result.add(cheese);
-        result.add(butter);
-        result.add(peanutButter);
-        result.add(mixedSalad);
-        result.add(ham);
-        result.add(chicken);
-        result.add(eggs);
-        return result;
-    }
-
-    private ArrayList<Item> GetItemList() {
-        ArrayList<Item> result = new ArrayList<>();
-        Item bread = new Item("bread", 10, IngredientUnit.SLICES, LocalDate.now().plusDays(3));
-        Item cheese = new Item("cheese", 10, IngredientUnit.SLICES, LocalDate.now().minusDays(5));
-        Item butter = new Item("butter", 250, IngredientUnit.GRAMS, LocalDate.now().plusMonths(3));
-        Item peanutButter = new Item("peanut butter", 250, IngredientUnit.GRAMS, LocalDate.now().plusYears(2));
-        Item mixedSalad = new Item("mixed salad", 500, IngredientUnit.GRAMS, LocalDate.now().plusDays(2));
-        result.add(bread);
-        result.add(cheese);
-        result.add(butter);
-        result.add(peanutButter);
-        result.add(mixedSalad);
-        return result;
-    }
 
 }
