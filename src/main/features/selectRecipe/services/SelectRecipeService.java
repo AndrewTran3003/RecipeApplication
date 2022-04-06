89ceec3cf73f08947fa5c7bfa10059183f86a5dd
Andrew Tran <Andrew@synercon.co>
2022-04-06 12:16:14 +1000

    restructure code

--- src/main/features/selectRecipe/services/SelectRecipeService.java
+++ src/main/features/selectRecipe/services/SelectRecipeService.java
@@ -1,4 +1,16 @@
 package features.selectRecipe.services;
 
+import helpers.StringHelper;
+import models.OperationResultMessage;
+import models.OperationResultStatus;
+import models.fridge.Fridge;
+import models.recipeList.Recipe;
+import models.recipeList.Recipes;
+import org.springframework.stereotype.Service;
+
+@Service
 public class SelectRecipeService {
+    public OperationResultMessage<Recipe> findRecipe(Fridge fridge, Recipes recipeList) {
+        return new OperationResultMessage<>(OperationResultStatus.SUCCESS, StringHelper.EMPTY_STRING,null);
+    }
 }
--- src/main/features/selectRecipe/tests/SelectRecipeServiceTest.java
+++ src/main/features/selectRecipe/tests/SelectRecipeServiceTest.java
@@ -1,10 +1,31 @@
 import features.selectRecipe.services.SelectRecipeService;
+import models.OperationResultMessage;
+import models.OperationResultStatus;
+import models.fridge.Fridge;
+import models.recipeList.Recipe;
+import models.recipeList.Recipes;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest(classes = SelectRecipeServiceTest.class)
 public class SelectRecipeServiceTest {
+    @Test
     public void canFindRecipe(){
         SelectRecipeService service = new SelectRecipeService();
+        Fridge fridge = getFridgeData();
+        Recipes recipeList = getRecipeList();
+        OperationResultMessage<Recipe> selectedRecipeResult = service.findRecipe(fridge,recipeList);
+        Assertions.assertEquals(OperationResultStatus.SUCCESS, selectedRecipeResult.getStatus());
+        Assertions.assertNotEquals(null, selectedRecipeResult.getResult());
 
+    }
+
+    private Recipes getRecipeList() {
+        return null;
+    }
+
+    private Fridge getFridgeData() {
+        return null;
     }
 }
